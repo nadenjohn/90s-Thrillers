653617a9aaa93574e967ad3b722670b9ec55c909
John <nadenjohn@gmail.com>
2022-02-21 16:41:01 -0800

    added function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const h2 = document.createElement("h2");
+/*const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h2);*/
+
+document.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("image2").style.display = "block";;
+}
+
